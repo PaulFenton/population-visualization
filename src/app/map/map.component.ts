4969099b083a7f9836125992671c7d9885c11549
PaulFenton <paulfenton@dal.ca>
2018-12-30 09:54:31 -0600

    https for tile server url

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -11,7 +11,7 @@ export class MapComponent implements OnInit {
   map;
   options = {
     layers: [
-      L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png')
+      L.tileLayer('https://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png')
     ],
     zoom: 1,
     center: L.latLng(51.7520, 1.2577)
