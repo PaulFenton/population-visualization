ea6546e7c5defbe9ca40dffa28c6b997ab5f419f
PaulFenton <paulfenton@dal.ca>
2018-12-30 09:44:05 -0600

    initial map hello world

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MapComponent } from './map/map.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //redirect to map component
+  {
+    path: '',
+    redirectTo: '/map',
+    pathMatch: 'full'
+  },
+  //main map component
+  {
+    path: 'map',
+    component: MapComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/map/map.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.css']
+})
+export class MapComponent implements OnInit {
+
+  map;
+  options = {
+    layers: [
+      L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png')
+    ],
+    zoom: 1,
+    center: L.latLng(51.7520, 1.2577)
+  };
+
+  constructor() { }
+
+  onMapReady(map) {
+
+    this.map = map;
+  }
+
+  ngOnInit() {
+  }
+
+}
